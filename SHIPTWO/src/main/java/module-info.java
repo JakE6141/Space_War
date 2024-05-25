dac43dbba1d80ffdb013a3d99afd9a9907508bb2
Jake D'Ambola <103600731+JakE6141@users.noreply.github.com>
2024-05-25 13:01:21 -0400

    Delete SHIPTWO directory

--- SHIPTWO/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.shipzz.gamez.shiptwo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.shipzz.gamez.shiptwo to javafx.fxml;
-    exports com.shipzz.gamez.shiptwo;
-}
